30d9b8e85545953d99859ee1a88821b69ef1e1ed
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-08-26 13:50:19 -0400

    Episode 2 Complete

--- tests/TranscriptionTest.php
+++ tests/TranscriptionTest.php
@@ -8,12 +8,30 @@ use PHPUnit\Framework\TestCase;
 class TranscriptionTest extends TestCase
 {
     /** @test */
-    function it_loads_a_vtt_file()
+    function it_loads_a_vtt_file_as_a_string()
     {
-        $transcription = Transcription::load(__DIR__ .'/stubs/basic-example.vtt');
+        $file = __DIR__ . '/stubs/basic-example.vtt';
 
-        $expected = file_get_contents(__DIR__ .'/stubs/basic-example.vtt');
+        $transcription = Transcription::load($file);
 
-        $this->assertEquals($expected, $transcription);
+        $this->assertStringContainsString('Here is a', $transcription);
+        $this->assertStringContainsString('example of a VTT file', $transcription);
+    }
+
+    /** @test */
+    function it_can_convert_to_an_array_of_lines()
+    {
+        $file = __DIR__ . '/stubs/basic-example.vtt';
+
+        $this->assertCount(4, Transcription::load($file)->lines());
+    }
+
+    /** @test */
+    function it_discards_irrelevant_lines_from_the_vtt_file()
+    {
+        $transcription = Transcription::load(__DIR__ . '/stubs/basic-example.vtt');
+
+        $this->assertStringNotContainsString('WEBVTT', $transcription);
+        $this->assertCount(4, $transcription->lines());
     }
 }
