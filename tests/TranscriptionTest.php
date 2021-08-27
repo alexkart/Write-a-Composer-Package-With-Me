9ea283db1ce7fac8907bf108fd379840bc06fcdb
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-08-27 14:53:30 -0400

    Episode 5 Complete

--- tests/TranscriptionTest.php
+++ tests/TranscriptionTest.php
@@ -8,23 +8,26 @@ use PHPUnit\Framework\TestCase;
 
 class TranscriptionTest extends TestCase
 {
+    protected Transcription $transcription;
+
+    protected function setUp(): void
+    {
+        $this->transcription = Transcription::load(
+            __DIR__ . '/stubs/basic-example.vtt'
+        );
+    }
+
     /** @test */
     function it_loads_a_vtt_file_as_a_string()
     {
-        $file = __DIR__ . '/stubs/basic-example.vtt';
-
-        $transcription = Transcription::load($file);
-
-        $this->assertStringContainsString('Here is a', $transcription);
-        $this->assertStringContainsString('example of a VTT file', $transcription);
+        $this->assertStringContainsString('Here is a', $this->transcription);
+        $this->assertStringContainsString('example of a VTT file', $this->transcription);
     }
 
     /** @test */
     function it_can_convert_to_an_array_of_line_objects()
     {
-        $file = __DIR__ . '/stubs/basic-example.vtt';
-
-        $lines = Transcription::load($file)->lines();
+        $lines = $this->transcription->lines();
 
         $this->assertCount(2, $lines);
 
@@ -34,22 +37,18 @@ class TranscriptionTest extends TestCase
     /** @test */
     function it_discards_irrelevant_lines_from_the_vtt_file()
     {
-        $transcription = Transcription::load(__DIR__ . '/stubs/basic-example.vtt');
-
-        $this->assertStringNotContainsString('WEBVTT', $transcription);
-        $this->assertCount(2, $transcription->lines());
+        $this->assertStringNotContainsString('WEBVTT', $this->transcription);
+        $this->assertCount(2, $this->transcription->lines());
     }
 
     /** @test */
     function it_renders_the_lines_as_html()
     {
-        $transcription = Transcription::load(__DIR__ . '/stubs/basic-example.vtt');
-
         $expected = <<<EOT
-<a href="?time=00:03">Here is a</a>
-<a href="?time=00:04">example of a VTT file.</a>
-EOT;
+            <a href="?time=00:03">Here is a</a>
+            <a href="?time=00:04">example of a VTT file.</a>
+            EOT;
 
-        $this->assertEquals($expected, $transcription->htmlLines());
+        $this->assertEquals($expected, $this->transcription->lines()->asHtml());
     }
 }
